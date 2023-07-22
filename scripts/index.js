35a441072701fcfdcf005a0624afa48001ae608b
Dillona25 <Dillyb00by49@gmail.com>
2023-07-21 22:59:19 -0500

    refactored card list

--- scripts/index.js
+++ scripts/index.js
@@ -35,11 +35,27 @@ const profileName = document.querySelector("#profile-name");
 const profileSubtitle = document.querySelector("#profile_subtitle");
 const modal_name = document.querySelector("#modal__name");
 const modal__subtitle = document.querySelector("#modal__subtitle");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardList = document.querySelector("#card-list");
 
 //Functions
 
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
+}
+
+function GetCardElement(CardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector("#cards-image");
+  const cardDescriptionEL = cardElement.querySelector("#cards-description");
+  //set the path to the image to the link field of the object
+  //set the image alt text to the name field of the object
+  //set the card title to the name field of the object, too
+  cardDescriptionEL.textContent = CardData.name;
+  //return the ready HTML element with the filled-in data
+  return cardElement;
+  cardList.append(cardElement);
 }
 
 //Event Handlers
@@ -64,3 +80,8 @@ ModalCloseButton.addEventListener("click", () => {
 });
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((CardData) => {
+  const cardElement = GetCardElement(CardData);
+  cardList.prepend(cardElement);
+});
