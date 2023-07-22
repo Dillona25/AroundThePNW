53bd9e232ded14b4da25491b1c9c47ffd129c3f8
Dillona25 <Dillyb00by49@gmail.com>
2023-07-21 22:13:23 -0500

    refactor JS

--- scripts/index.js
+++ scripts/index.js
@@ -25,10 +25,10 @@ const initialCards = [
   },
 ];
 
-//Elements Below
+//Elements
 
 const profileEditForm = document.querySelector("#profile-edit-modal");
-const profileEditButton = document.querySelector("#profile-edit-button"); //Recommended giving it an ID as that will never chnage!
+const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const ModalCloseButton = document.querySelector("#modal-close");
 const profileName = document.querySelector("#profile-name");
@@ -36,25 +36,31 @@ const profileSubtitle = document.querySelector("#profile_subtitle");
 const modal_name = document.querySelector("#modal__name");
 const modal__subtitle = document.querySelector("#modal__subtitle");
 
+//Functions
+
 function closePopup() {
   profileEditModal.classList.remove("modal_opened");
 }
 
+//Event Handlers
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileName.textContent = modal__name.value;
+  profileSubtitle.textContent = modal__subtitle.value;
+  closePopup();
+}
+
+//Event listeners
+
 profileEditButton.addEventListener("click", () => {
   modal__name.value = profileName.textContent;
   modal__subtitle.value = profileSubtitle.textContent;
-  profileEditModal.classList.add("modal_opened"); // () => is the second argument and is a function.
+  profileEditModal.classList.add("modal_opened");
 });
-
-//Examples of opening and closing a modal
 
 ModalCloseButton.addEventListener("click", () => {
   profileEditModal.classList.remove("modal_opened");
 });
 
-profileEditForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  profileName.textContent = modal__name.value;
-  profileSubtitle.textContent = modal__subtitle.value;
-  closePopup();
-});
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
