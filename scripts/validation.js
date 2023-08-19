1e67d2fe187cef9e20431877a849a710a63f0009
Dillona25 <Dillyb00by49@gmail.com>
2023-08-19 13:11:33 -0500

    Added disabled button

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,42 +1,66 @@
-//* Elements
-const formElement = document.querySelector(".modal__form");
-const formInput = formElement.querySelector(".modal__input");
-const formError = document.querySelector("modal__input_type_error");
-
-//* Event Listeners
-formElement.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-});
-
-const showError = (input, errorMessage) => {
-  input.classList.add(".modal__input_type_error");
-  formError.textContent = errorMessage;
-  formError.classList.add(".modal__error_visbile");
+const showErrorMessage = (input, { errorClass, inputErrorClass }) => {
+  const errorMessage = document.querySelector(`#${input.id}-error`);
+  errorMessage.textContent = input.validationMessage;
+  errorMessage.classList.add(errorClass);
+  input.classList.add(inputErrorClass);
 };
 
-const hideError = (input) => {
-  input.classList.remove("modal__input_type_error");
-  formError.classList.remove("modal__error_visible");
-  formError.textContent = "";
+const hideErrorMessage = (input, { errorClass, inputErrorClass }) => {
+  const errorMessage = document.querySelector(`#${input.id}-error`);
+  errorMessage.classList.remove(errorClass);
+  errorMessage.classList.remove(inputErrorClass);
+  errorMessage.textContent = "";
 };
 
-const checkInputValidity = () => {
-  if (!formInput.validity.valid) {
-    showError(formInput, formInput.validationMessage);
+const checkInputValidity = (input, options) => {
+  if (input.validity.valid) {
+    hideErrorMessage(input, options);
   } else {
-    hideError(formInput);
+    showErrorMessage(input, options);
   }
 };
 
-formInput.addEventListener("input", function (evt) {
-  console.log(evt.target.validity);
-});
+const toggleButtonState = (inputs, button, options) => {
+  const isValid = inputs.every((input) => input.validity.valid);
+  console.log(isValid, button);
+  if (isValid) {
+    button.classList.remove(options.inactiveButtonClass);
+  } else {
+    button.classList.add(options.inactiveButtonClass);
+  }
+};
+
+const enableValidation = ({
+  formSelector,
+  inputSelector,
+  submitButtonSelector,
+  ...options
+}) => {
+  const formElements = [...document.querySelectorAll(formSelector)];
+  console.log(formElements);
+  formElements.forEach((form) => {
+    form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    const formInputs = [...document.querySelectorAll(inputSelector)];
+    const formButton = form.querySelector(submitButtonSelector);
+    console.log(formInputs);
+    formInputs.forEach((input) => {
+      input.addEventListener("input", () => {
+        checkInputValidity(input, options);
+        toggleButtonState(formInputs, formButton, options);
+      });
+    });
+  });
+};
+
 //* Objects
-const config = {
+
+enableValidation({
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__button",
   inactiveButtonClass: "modal__button_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
-};
+});
