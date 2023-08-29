32fe18347e2a87d9a74f6efb8590f3904192647b
Dillona25 <Dillyb00by49@gmail.com>
2023-08-29 16:16:39 -0500

    refactored JavaScript methods

--- components/card.js
+++ components/card.js
@@ -1,3 +1,13 @@
 export default class Card {
-  constructor(cardData) {}
+  constructor({ name, link }) {
+    this._name = name;
+    this._link = link;
+    console.log(name);
+  }
+
+  getCard() {
+    // get the card view
+    // set event listeners
+    //return card
+  }
 }
